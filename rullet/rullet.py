a22986b85f98833f43049da939140a60a8ab00fc
Minsoo Park <minsoo1003@gmail.com>
2015-02-04 15:35:14 +0900

    process with no args

--- rullet/rullet.py
+++ rullet/rullet.py
@@ -3,8 +3,20 @@
 import random, codecs, os, sys
 
 def run():
-  args = sys.argv[1:len(sys.argv)]
-  print args[(int) (random.random() * len(args))]
+  if not sys.argv or len(sys.argv) == 1:
+    help()
+  else:
+    args = sys.argv[1:len(sys.argv)]
+    print args[(int) (random.random() * len(args))]
+
+
+def help():
+  print ''
+  print '[How to use]'
+  print ''
+  print '$ rullet foo bar baz ...'
+  print '> bar'
+  print ''
 
 
 if __name__ == '__main__':
